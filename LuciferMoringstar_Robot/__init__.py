809443086edcd17da0067d23ce205bc5c2494557
Gokul Bijoy <81373116+carlmax-here@users.noreply.github.com>
2021-09-30 12:37:39 +0530

    Update __init__.py

--- LuciferMoringstar_Robot/__init__.py
+++ LuciferMoringstar_Robot/__init__.py
@@ -25,19 +25,17 @@ Bot Owner Only
 /logger  Get Logs
 /delete Delete File From Database
 
-[➡️How To Add Old Files For database](https://youtu.be/mtu5hgW-l_o)
-
-@Mo_Tech_YT
+<b>@CinemaPedikaGroup</b>
 """
 
 ABOUT = """
 ➥ Name :  Auto Filter With IMDB
     
-➥ Creator : [Muhammed RK](t.me/Mrk_YT)
+➥ Creator : [CINEMA PEDIKA ADMINS](t.me/CinemaPedikaGroup)
 
 ➥ Language : Python3
 
 ➥ Library : Pyrogram Asyncio 1.13.0
 
-➥ Source Code : [Click Me](https://github.com/PR0FESS0R-99/LuciferMoringstar_Robot)
+➥ MAIN CHANNEL : [Click Me](https://t.me/CP_S4U_Links)
 """
