da5ddc4718fa8764f45b523ca0d88921f225f16e
Gokul Bijoy <81373116+carlmax-here@users.noreply.github.com>
2021-10-18 16:22:38 +0530

    Update Main.py

--- LuciferMoringstar_Robot/Filter/Main.py
+++ LuciferMoringstar_Robot/Filter/Main.py
@@ -125,7 +125,7 @@ async def group(client, message):
         if files:
             for file in files:
                 file_id = file.file_id
-                filename = f"»【{get_size(file.file_size)}】● 🎬 {file.file_name}"
+                filename = f"»【{get_size(file.file_size)}】 ● 🎬 {file.file_name}"
                 btn.append(
                     [InlineKeyboardButton(text=f"{filename}", url=f"https://telegram.dog/{nyva}?start=pr0fess0r_99_-_-_-_{file_id}")]
                 )
